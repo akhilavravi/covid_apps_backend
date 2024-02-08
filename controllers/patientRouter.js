97e0d944676cafcb84abe8e04e1c005b22744a35
akhilavravi <akhilavraveendran0005@gmail.com>
2024-02-08 15:56:37 +0530

    search update

--- controllers/patientRouter.js
+++ controllers/patientRouter.js
@@ -18,4 +18,10 @@ router.get("/viewall",async(req,res)=>{
     res.json(data)
 })
 
+router.post("/patient_search",async(req,res)=>{
+    let input=req.body
+    let data=await patientModel.find(input)
+    res.json(data)   //reading value
+})
+
 module.exports=router
